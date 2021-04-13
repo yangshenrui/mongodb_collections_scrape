4cdddb862b83b2c74930fef35d7e5706561bca62
SHENRUI <22@qq.com>
2021-04-13 16:48:22 +0800

    client_java 1

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -1,14 +1,34 @@
 package com.example.springboot;
 
+import com.example.springboot.client.YourCustomCollector;
+import com.example.springboot.client.YourCustomCollector2;
+import com.example.springboot.dal.ExporterRepository;
+import com.example.springboot.model.Exporter;
+import io.prometheus.client.exporter.HTTPServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Application {
+import java.io.IOException;
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+	@Autowired
+	private ExporterRepository exporterRepository;
+
+	public static void main(String[] args)  throws IOException {
 		SpringApplication.run(Application.class, args);
 
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		new YourCustomCollector().register();
+		new YourCustomCollector2().register();
+		HTTPServer server = new HTTPServer(1234);
+		Application application = new Application();
+		System.out.println(exporterRepository.findByAge("100"));
+		System.out.println(exporterRepository.findAll());
+	}
 }
--- src/main/java/com/example/springboot/dal/ExporterRepository.java
+++ src/main/java/com/example/springboot/dal/ExporterRepository.java
@@ -2,8 +2,9 @@ package com.example.springboot.dal;
 
 import com.example.springboot.model.Exporter;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface ExporterRepository extends MongoRepository<Exporter, String> {
-
+    Exporter findByAge(String age);
 }
